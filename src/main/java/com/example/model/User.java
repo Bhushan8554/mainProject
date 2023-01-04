9b1cf3ab75250cdd414e8282b86bee3cc038a94b
bhushan8554 <bhushantaru4297@gmail.com>
2023-01-04 20:36:42 +0530

    adding user class

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,5 +1,14 @@
 package com.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	private String mobileNumber;
+	private String username;
+	
+	
 }
