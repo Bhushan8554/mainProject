25dc20deb076f3344504b7f9c652f79b5635c035
bhushan8554 <bhushantaru4297@gmail.com>
2023-01-04 20:38:16 +0530

    adding details in user class

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -2,13 +2,16 @@ package com.example.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class User {
 
 	@Id
 	private String mobileNumber;
 	private String username;
-	
+	private String email;
+	private String password;
 	
 }
